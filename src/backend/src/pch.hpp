a3c59e8a1839bbe49941aff382691e6e1e3566c7
BoheSama1999 <Zhibohemain@gmail.com>
2026-03-11 21:19:05 +0800

    build(pch) : 添加预编译头文件 加快二次编译速度

---
+++ src/backend/src/pch.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+// C++ STL
+#include <chrono>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <regex>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+// nlohmann/json (linked by all targets)
+#include <nlohmann/json.hpp>
