5d6fcc61a6ad457dc4eede1a75d5abd85c9677aa
BoheSama1999 <Zhibohemain@gmail.com>
2026-03-12 02:01:03 +0800

    test: implement Logger module tests for log writing and cleanup

--- src/backend/tests/utils/test_logger.cpp
+++ src/backend/tests/utils/test_logger.cpp
@@ -6,8 +6,12 @@
 #include "../test_utils.hpp"
 #include "utils/logger.hpp"
 
+#include <chrono>
 #include <filesystem>
+#include <fstream>
 #include <gtest/gtest.h>
+#include <iomanip>
+#include <sstream>
 
 namespace quickmemes { namespace testing {
 
@@ -25,15 +29,74 @@ protected:
 };
 
 TEST_F(LoggerTest, LogWrite_AboveMinLevel_WritesToFile) {
-	// TODO: implement
+	auto &logger = ::quickmemes::Logger::get();
+	logger.initialize(tempDir_->getPath(), ::quickmemes::LogLevel::DEBUG, false);
+
+	const std::string module  = "logtest1"; // unique module to avoid cached streams
+	const std::string message = "hello logger";
+	logger.log(::quickmemes::LogLevel::INFO, module, message);
+
+	// build expected log file path: <logDir>/<module>-<YYYY-MM-DD>.log
+	auto    now   = std::chrono::system_clock::now();
+	auto    timeT = std::chrono::system_clock::to_time_t(now);
+	std::tm tm{};
+	localtime_r(&timeT, &tm);
+	std::ostringstream date;
+	date << std::put_time(&tm, "%Y-%m-%d");
+	const std::string logPath = tempDir_->getPath() + "/" + module + "-" + date.str() + ".log";
+
+	ASSERT_TRUE(std::filesystem::exists(logPath));
+	std::ifstream ifs(logPath);
+	ASSERT_TRUE(ifs.is_open());
+	std::stringstream buffer;
+	buffer << ifs.rdbuf();
+	ASSERT_NE(buffer.str().find(message), std::string::npos);
 }
 
 TEST_F(LoggerTest, LogWrite_BelowMinLevel_IgnoresLog) {
-	// TODO: implement
+	auto &logger = ::quickmemes::Logger::get();
+	logger.initialize(tempDir_->getPath(), ::quickmemes::LogLevel::WARN, false);
+
+	const std::string module  = "logtest2";
+	const std::string message = "should be filtered";
+	logger.log(::quickmemes::LogLevel::DEBUG, module, message);
+
+	auto    now   = std::chrono::system_clock::now();
+	auto    timeT = std::chrono::system_clock::to_time_t(now);
+	std::tm tm{};
+	localtime_r(&timeT, &tm);
+	std::ostringstream date;
+	date << std::put_time(&tm, "%Y-%m-%d");
+	const std::string logPath = tempDir_->getPath() + "/" + module + "-" + date.str() + ".log";
+
+	ASSERT_FALSE(std::filesystem::exists(logPath));
 }
 
 TEST_F(LoggerTest, CleanOldLogs_RemovesExpiredFiles) {
-	// TODO: implement
+	const std::string logDir = tempDir_->getPath();
+	const std::string oldLog = logDir + "/old.log";
+	const std::string newLog = logDir + "/new.log";
+
+	{
+		std::ofstream ofsOld(oldLog);
+		ofsOld << "old";
+	}
+	{
+		std::ofstream ofsNew(newLog);
+		ofsNew << "new";
+	}
+
+	const auto nowFileClock = std::filesystem::file_time_type::clock::now();
+	std::filesystem::last_write_time(oldLog, nowFileClock - std::chrono::hours(24 * 5)); // 5 days ago
+	std::filesystem::last_write_time(newLog, nowFileClock - std::chrono::hours(24));     // 1 day ago
+
+	auto &logger = ::quickmemes::Logger::get();
+	logger.initialize(logDir, ::quickmemes::LogLevel::INFO, false);
+
+	int removed = logger.cleanOldLogs(3);
+	EXPECT_EQ(removed, 1);
+	EXPECT_FALSE(std::filesystem::exists(oldLog));
+	EXPECT_TRUE(std::filesystem::exists(newLog));
 }
 
 }} // namespace quickmemes::testing
