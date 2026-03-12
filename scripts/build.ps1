d44e98a93c51de7d5e590d2bb681b991955d30fe
BoheSama1999 <Zhibohemain@gmail.com>
2026-03-13 02:10:09 +0800

    test(CI): 尝试修复ci环境windows中缺少openssl的问题

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -51,7 +51,7 @@ if ($env:GITHUB_ACTIONS -eq "true") {
     Write-Host "Running inside GitHub Actions. Injecting OpenSSL path..."
     $cmakeArgs += "-DOPENSSL_ROOT_DIR=C:\Program Files\OpenSSL-Win64"
     $cmakeArgs += "-DOPENSSL_INCLUDE_DIR=C:\Program Files\OpenSSL-Win64\include"
-    $cmakeArgs += "-DOPENSSL_CRYPTO_LIBRARY=C:\Program Files\OpenSSL-Win64\lib\libcrypto.lib"
+    $cmakeArgs += "-DOPENSSL_CRYPTO_LIBRARY=C:\Program Files\OpenSSL-Win64\lib"
 }
 
 & cmake @cmakeArgs
